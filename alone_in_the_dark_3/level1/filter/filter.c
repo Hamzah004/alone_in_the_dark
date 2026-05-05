e89b9c6089d479154392cb7455a950fa82185bfe
Hamzah004 <hamzahbaniata75@gmail.com>
2026-05-05 13:30:05 +0300

    added new filter solve

--- alone_in_the_dark_3/level1/filter/filter.c
+++ alone_in_the_dark_3/level1/filter/filter.c
@@ -1,50 +1,78 @@
- #define _GNU_SOURCE
-                            #include <string.h>
-#include <errno.h>
-#include <fcntl.h>
+#define _GNU_SOURCE
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-int	main(int argc, char **argv)
+void ft_filter(char *buffer, const char *target)
 {
-	char	buffer[5000];
-	char	*tmp;
-	int	bytes_read;
-	int	i;
-	int	j;
-	int	argv_len;
-	int	start;
+    int i = 0;
+    int target_len = strlen(target);
+    int j, k;
 
+    while (buffer[i])
+    {
+        j = 0;
+        while (target[j] && (buffer[i + j] == target[j]))
+            j++;
 
-	if (argc != 2)
-		return (1);
-	argv_len = strlen(argv[1]);
-	while ((bytes_read = read(0, buffer, sizeof(buffer))) > 0)
-	{
-		i = 0;
-		while (i < bytes_read)
-		{
-			tmp = memmem(buffer + i, bytes_read - i, argv[1], argv_len);
-			if (!tmp)
-			{
-				write(1, buffer + i, bytes_read - i);
-				break ;
-			}
-			start = tmp - buffer;
-			write(1, buffer + i, start - i);
-			j = 0;
-			while (j < argv_len)
-			{
-				write(1, "*", 1);
-				j++;
-			}
-			i = start + argv_len;
-		}
-	}
-	if (bytes_read < 0)
-	{
-		perror("Error:");
-		return (1);
-	}
-	return (0);
+        if (j == target_len)
+        {
+            k = 0;
+            while (k < target_len)
+            {
+                write(1, "*", 1);
+                k++;
+            }
+            i += target_len;
+        }
+        else
+        {
+            write(1, &buffer[i], 1);
+            i++;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2 || argv[1][0] == '\0')
+        return 1;
+    char temp[BUFFER_SIZE];
+    char *result = NULL;
+    char *buffer;
+    int total_read = 0;
+    ssize_t bytes;
+
+    while ((bytes = read(0, temp, BUFFER_SIZE)) > 0)
+    {
+        buffer = realloc(result, total_read + bytes + 1);
+        if (!buffer)
+        {
+            free(result);
+            perror("realloc");
+            return 1;
+        }
+        result = buffer;
+        memmove(result + total_read, temp, bytes);
+        total_read += bytes;
+        result[total_read] = '\0';
+    }
+    if (bytes < 0)
+    {
+        perror("read");
+        free(result);
+        return 1;
+    }
+    if (!result)
+        return 0;
+    ft_filter(result, argv[1]);
+
+    free(result);
+    return 0;
 }
